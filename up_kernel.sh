65342b65c3f51c0d19bcba4651294c89d1c67fcb
m1911 <gming1911@gmail.com>
2018-09-28 11:43:38 +0800

    删除脚本

--- up_kernel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#升级内核
-rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-rpm -Uvh https://mirrors.tuna.tsinghua.edu.cn/elrepo/extras/el7/x86_64/RPMS/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-lt -y #安装稳定内核
-grub2-set-default 0 #设置启动内核
-reboot
